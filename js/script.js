dd5078b9a0e74f35cc73dc2b06f98d941521b7af
XavierThe3rd <tommy@kimtom.net>
2018-12-04 16:39:47 -0500

    javascript changes for new improved header

--- js/script.js
+++ js/script.js
@@ -3,35 +3,64 @@ console.log(window.innerWidth > 1000);
 window.onscroll = function() {
     if (window.innerWidth > 1075) {  
         document.getElementById("mySidenav").style.display = "none"
-        document.getElementById("mySidenav2").style.display = "none";
+        document.getElementById("mySidenav2").style.display = "none"
+        document.getElementById("navWrap").style.position ="inherit"
+        document.getElementById("navTitle").style.position ="relative";
 
         if (window.pageYOffset > 98) {
             document.getElementById("nav2").style.display = "block";
         } 
 
+            
         else {
-            document.getElementById("nav2").style.display = "none";
+            document.getElementById("nav2").style.display = "none"
+            document.getElementById("navBar").style.top ="98px";
         }
-    } 
+    }
+
 
     else {
         document.getElementById("mySidenav").style.display = "flex"
         document.getElementById("mySidenav2").style.display = "flex";
 
-        if (window.pageYOffset > 168) {
-            document.getElementById("nav2").style.display = "flex"
-            document.getElementById("mySidenav2").style.display = "flex"
-            document.getElementById("mySidenav").style.display = "none"
-            document.getElementById("mySidenav").style.height = "0%"
-            document.getElementById("mySidenav").style.padding = "0px";
-        } 
+        if (window.pageYOffset > 70) {
+            document.getElementById("navWrap").style.position = "fixed"
+            document.getElementById("navWrap").style.top = "0"
+            document.getElementById("navTitle").style.position = "relative"
+            document.getElementById("navBar").style.top = "98px"
+            document.getElementById("mySidenav").style.position = "inherit";
 
-        else {
-            document.getElementById("nav2").style.display ="none"
-            document.getElementById("mySidenav2").style.display = "none"
-            document.getElementById("mySidenav").style.display = "flex"
-            document.getElementById("mySidenav2").style.height = "0%"
-            document.getElementById("mySidenav2").style.padding = "0px";
+            if (window.pageYOffset > 300) {
+                document.getElementById("mySidenav2").style.display = "flex"
+                document.getElementById("mySidenav").style.height = "0%"
+                document.getElementById("mySidenav").style.padding = "0px"
+                document.getElementById("navWrap").style.position ="absolute"
+                document.getElementById("navWrap").style.top = "300px"
+                document.getElementById("navTitle").style.position = "relative";
+                
+                if (window.pageYOffset > 398) {
+                    document.getElementById("nav2").style.display = "flex";
+                    document.getElementById("mySidenav2").style.right = "12%";
+                }
+                else {
+                    document.getElementById("nav2").style.display = "none";
+                }
+            }
+            else {
+                document.getElementById("nav2").style.display ="none"
+                document.getElementById("mySidenav2").style.display = "none"
+                document.getElementById("mySidenav").style.top = "168px";
+            }
+        
+        } 
+            
+        else {  
+            document.getElementById("navWrap").style.position = "inherit"
+            document.getElementById("navBar").style.top = "168px"
+            document.getElementById("mySidenav").style.position = "absolute"
+            document.getElementById("navTitle").style.position = "absolute"
+            document.getElementById("navTitle").style.width = "100%"
+            document.getElementById("mySidenav2").style.display = "none";
         }
     }
 }
